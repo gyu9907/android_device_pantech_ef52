1b165991d7644c55515b0bf35a9d499c972d1a6c
gyu9907 <gyu9907@gmail.com>
2026-08-16 15:45:00 +0900

    ef52: pin pre-RPMB qseecomd blob

--- extract-files.sh
+++ extract-files.sh
@@ -2,6 +2,7 @@
 
 VENDOR=pantech
 DEVICE=ef52
+QSEECOMD_SHA256=5cbf7b7a9a9a3b5ba45bc9d7978f410f3d1ce20fca19f821e1b8b7b44ab80d98
 
 BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
 rm -rf $BASE/*
@@ -12,6 +13,9 @@ for FILE in `cat proprietary-blobs.txt | grep -v ^# | grep -v ^$ `; do
         mkdir -p $BASE/$DIR
     fi
     adb pull /system/$FILE $BASE/$FILE
+    if [ "$FILE" = "bin/qseecomd" ]; then
+        echo "$QSEECOMD_SHA256  $BASE/$FILE" | sha256sum -c - || exit 1
+    fi
 done
 ./copy_from_target.sh
 ./setup-makefiles.sh
